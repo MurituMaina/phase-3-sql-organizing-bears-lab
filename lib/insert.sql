fad5dbd0f1ebea82730547b33f57e4217d8f3ca9
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-26 23:18:00 +0300

    complete tasks

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+"Mr. Chocolate", 3, "M", "Brown", "jovial", 1 );
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+"Rowdy", 3, "M", "Brown", "Angered", 1 );
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+"Tabitha", 3, "F", "Brown", "hyper", 1 );
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+"Sergeant Brown", 3, "M", "Brown", "sad", 1 );
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+"Melissa", 3, "F", "Brown", "jovial", 1 );
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+"Grinch", 3, "M", "Brown", "Dull", 1 );
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+"Wendy", 3, "F", "Brown", "happy", 1 );
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+NULL, 3, "F", "Brown", "moody", 1);
